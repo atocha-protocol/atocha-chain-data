86f70141c91cd72151a69f6b9d11d8b4231daa6e
h12 <630086711@qq.com>
2022-02-26 09:15:12 +0800

    Update subquery for atocha.

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,5 +1,11 @@
 import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
-import {PuzzleCreatedEvent, AnswerCreatedEvent, ChallengeDepositEvent, ChallengeStatusChangeEvent} from "../types";
+import {
+    PuzzleCreatedEvent,
+    AnswerCreatedEvent,
+    ChallengeDepositEvent,
+    ChallengeStatusChangeEvent,
+    PuzzleStatusChangeEvent, TakeTokenRewardEvent, TakePointRewardEvent
+} from "../types";
 import {Balance} from "@polkadot/types/interfaces";
 
 
@@ -8,8 +14,9 @@ export async function handlePuzzleCreatedEvent(event: SubstrateEvent): Promise<v
     const record = new PuzzleCreatedEvent(puzzle_hash.toHuman().toString());
     record.who = who.toString();
     record.puzzle_hash = puzzle_hash.toHuman().toString();
-    record.create_bn = create_bn.toString();
-    record.event_bn = event.block.block.header.number.toString();
+    record.create_bn = BigInt(create_bn.toString());
+    record.event_bn = BigInt(event.block.block.header.number.toString());
+    record.event_hash = event.block.block.hash.toHuman().toString()
     record.deposit = (deposit as Balance).toBigInt();
     await record.save();
 }
@@ -20,8 +27,9 @@ export async function handleAnswerCreatedEvent(event: SubstrateEvent): Promise<v
     const record = new AnswerCreatedEvent(puzzle_hash.toHuman().toString());
     record.who = who.toString();
     record.answer_content = answer_content.toHuman().toString();
-    record.create_bn = create_bn.toString();
-    record.event_bn = event.block.block.header.number.toString();
+    record.create_bn = BigInt(create_bn.toString());
+    record.event_bn = BigInt(event.block.block.header.number.toString());
+    record.event_hash = event.block.block.hash.toHuman().toString()
     record.puzzle_hash = puzzle_hash.toHuman().toString();
     record.puzzle_infoId = puzzle_hash.toHuman().toString();
     record.result_type = result_type.toHuman().toString();
@@ -34,7 +42,8 @@ export async function handleChallengeDepositEvent(event: SubstrateEvent): Promis
     const record = new ChallengeDepositEvent(`${event.block.block.header.number.toString()}-${event.idx}`);
     record.who = who.toString();
     record.puzzle_hash = puzzle_hash.toHuman().toString();
-    record.event_bn = event.block.block.header.number.toString();
+    record.event_bn = BigInt(event.block.block.header.number.toString());
+    record.event_hash = event.block.block.hash.toHuman().toString()
     record.deposit = (deposit as Balance).toBigInt();
     record.deposit_type = deposit_type.toHuman().toString();
     record.puzzle_infoId = puzzle_hash.toHuman().toString();
@@ -46,16 +55,55 @@ export async function handleChallengeStatusChangeEvent(event: SubstrateEvent): P
     await makeSurePuzzleCreated(puzzle_hash.toHuman().toString());
     const record = new ChallengeStatusChangeEvent(`${event.block.block.header.number.toString()}-${event.idx}`);
 
-    record.event_bn = event.block.block.header.number.toString();
+    record.event_bn = BigInt(event.block.block.header.number.toString());
+    record.event_hash = event.block.block.hash.toHuman().toString()
     logger.info(`request_data C::#### ${stauts_info.toString()}, ${stauts_info.toHuman()}`);
     for(const key in stauts_info.toHuman() as any) {
         record.challenge_status = key;
-        record.change_bn = stauts_info.toHuman()[key];
+        record.change_bn = BigInt(stauts_info.toHuman()[key]);
     }
     logger.info(`record = ${record}`);
     record.puzzle_infoId = puzzle_hash.toHuman().toString();
     await record.save();
 }
+
+
+export async function handlePuzzleStatusChangeEvent(event: SubstrateEvent): Promise<void> {
+    const {event: {data: [puzzle_hash, stauts_info]}} = event;
+    await makeSurePuzzleCreated(puzzle_hash.toHuman().toString());
+    const record = new PuzzleStatusChangeEvent(`${event.block.block.header.number.toString()}-${event.idx}`);
+    record.event_bn = BigInt(event.block.block.header.number.toString());
+    record.event_hash = event.block.block.hash.toHuman().toString()
+    record.puzzle_infoId = puzzle_hash.toHuman().toString();
+    record.puzzle_status = stauts_info.toString();
+    await record.save();
+}
+
+// handleTakeTokenRewardEvent
+export async function handleTakeTokenRewardEvent(event: SubstrateEvent): Promise<void> {
+    const {event: {data: [puzzle_hash, take_token, fee]}} = event;
+    await makeSurePuzzleCreated(puzzle_hash.toHuman().toString());
+    const record = new TakeTokenRewardEvent(`${puzzle_hash.toHuman().toString()}`);
+    record.event_bn = BigInt(event.block.block.header.number.toString());
+    record.event_hash = event.block.block.hash.toHuman().toString()
+    record.puzzle_infoId = puzzle_hash.toHuman().toString();
+    record.token = (take_token as Balance).toBigInt();
+    record.fee = (fee as Balance).toBigInt();
+    await record.save();
+}
+
+export async function handleTakePointRewardEvent(event: SubstrateEvent): Promise<void> {
+    const {event: {data: [puzzle_hash, take_token, fee]}} = event;
+    await makeSurePuzzleCreated(puzzle_hash.toHuman().toString());
+    const record = new TakePointRewardEvent(`${puzzle_hash.toHuman().toString()}`);
+    record.event_bn = BigInt(event.block.block.header.number.toString());
+    record.event_hash = event.block.block.hash.toHuman().toString()
+    record.puzzle_infoId = puzzle_hash.toHuman().toString();
+    record.point = (take_token as Balance).toBigInt();
+    record.fee = (fee as Balance).toBigInt();
+    await record.save();
+}
+
 
 async function makeSurePuzzleCreated(puzzle_hash:string):Promise<void> {
     let puzzleCreated = await PuzzleCreatedEvent.get(puzzle_hash);
@@ -65,34 +113,5 @@ async function makeSurePuzzleCreated(puzzle_hash:string):Promise<void> {
         await puzzleCreated.save();
     }
 }
-
-
-
-// export async function handleBlock(block: SubstrateBlock): Promise<void> {
-//     //Create a new starterEntity with ID using block hash
-//     let record = new StarterEntity(block.block.header.hash.toString());
-//     //Record block number
-//     record.field1 = block.block.header.number.toNumber();
-//     await record.save();
-// }
-//
-// export async function handleEvent(event: SubstrateEvent): Promise<void> {
-//     const {event: {data: [account, balance]}} = event;
-//     //Retrieve the record by its ID
-//     const record = await StarterEntity.get(event.block.block.header.hash.toString());
-//     record.field2 = account.toString();
-//     //Big integer type Balance of a transfer event
-//     record.field3 = (balance as Balance).toBigInt();
-//     await record.save();
-// }
-//
-// export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-//     const record = await StarterEntity.get(extrinsic.block.block.header.hash.toString());
-//     //Date type timestamp
-//     record.field4 = extrinsic.block.timestamp;
-//     //Boolean tyep
-//     record.field5 = true;
-//     await record.save();
-// }
 
 
