4b56329677470cabe5436060020aa4697ae5481c
h12 <630086711@qq.com>
2022-03-02 09:38:17 +0800

    Add deposit events support.

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -4,9 +4,9 @@ import {
     AnswerCreatedEvent,
     ChallengeDepositEvent,
     ChallengeStatusChangeEvent,
-    PuzzleStatusChangeEvent, TakeTokenRewardEvent, TakePointRewardEvent
+    PuzzleStatusChangeEvent, TakeTokenRewardEvent, TakePointRewardEvent, PuzzleDepositEvent
 } from "../types";
-import {Balance} from "@polkadot/types/interfaces";
+import {Balance, BlockNumber} from "@polkadot/types/interfaces";
 
 
 export async function handlePuzzleCreatedEvent(event: SubstrateEvent): Promise<void> {
@@ -17,7 +17,7 @@ export async function handlePuzzleCreatedEvent(event: SubstrateEvent): Promise<v
     record.create_bn = BigInt(create_bn.toString());
     record.event_bn = BigInt(event.block.block.header.number.toString());
     record.event_hash = event.block.block.hash.toHuman().toString()
-    record.deposit = (deposit as Balance).toBigInt();
+    record.owner_deposit = (deposit as Balance).toBigInt();
     await record.save();
 }
 
@@ -60,9 +60,11 @@ export async function handleChallengeStatusChangeEvent(event: SubstrateEvent): P
     logger.info(`request_data C::#### ${stauts_info.toString()}, ${stauts_info.toHuman()}`);
     for(const key in stauts_info.toHuman() as any) {
         record.challenge_status = key;
-        record.change_bn = BigInt(stauts_info.toHuman()[key]);
+        // logger.info(`stauts_info..toJSON()()[key] = ${(stauts_info.toHuman()[key] as Balance).toBigInt()}`, );
+        let change_bn = stauts_info.toHuman()[key].replace(',','');
+        record.change_bn = BigInt(change_bn);
     }
-    logger.info(`record = ${record}`);
+    // logger.info(`record = ${record}`);
     record.puzzle_infoId = puzzle_hash.toHuman().toString();
     await record.save();
 }
@@ -104,6 +106,19 @@ export async function handleTakePointRewardEvent(event: SubstrateEvent): Promise
     await record.save();
 }
 
+// handleAdditionalSponsorshipEvent rename to handlePuzzleDepositEvent
+export async function handlePuzzleDepositEvent(event: SubstrateEvent): Promise<void> {
+    const {event: {data: [puzzle_hash, who, deposit, tip]}} = event;
+    await makeSurePuzzleCreated(puzzle_hash.toHuman().toString());
+    const record = new PuzzleDepositEvent(`${event.block.block.header.number.toString()}-${event.idx}`);
+    record.event_bn = BigInt(event.block.block.header.number.toString());
+    record.event_hash = event.block.block.hash.toHuman().toString()
+    record.puzzle_infoId = puzzle_hash.toHuman().toString();
+    record.who = who.toString();
+    record.deposit = (deposit as Balance).toBigInt();
+    record.tip = tip?tip.toHuman().toString():'';
+    await record.save();
+}
 
 async function makeSurePuzzleCreated(puzzle_hash:string):Promise<void> {
     let puzzleCreated = await PuzzleCreatedEvent.get(puzzle_hash);
